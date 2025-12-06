1ed8ad2a647f4abc0f1a7bd8be326be97641bc71
oktiaraazzahra <11231076@student.itk.ac.id>
2025-12-06 08:23:13 +0800

    Remove unnecessary blank lines in pembimbingController.js

--- controllers/pembimbingController.js
+++ controllers/pembimbingController.js
@@ -3,7 +3,6 @@ const conn = require('../config/db');
 const giveFeedback = (req, res) => {
   const dosen_id = req.user.id; // otomatis dari token dosen
   const { proposal_id, feedback, new_status_id } = req.body;
-  
   const query = `CALL sp_give_feedback(?, ?, ?, ?, @message); SELECT @message AS message;`;
   conn.query(query, [proposal_id, dosen_id, feedback, new_status_id], (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
@@ -14,7 +13,6 @@ const giveFeedback = (req, res) => {
 
 const getProposalsByDosen = (req, res) => {
   const dosen_id = req.user.id; // otomatis dari token dosen, tidak perlu di params
-  
   const query = `CALL sp_get_proposal_bimbingan(?);`;
   conn.query(query, [dosen_id], (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
